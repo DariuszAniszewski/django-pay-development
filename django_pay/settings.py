6c45324f01dd048d5a07ea24f53e9011b63554ee
Dariusz Aniszewski <dariusz.aniszewski@polidea.com>
2015-05-05 18:31:43 +0200

    added DjangoPayMiddleware

--- django_pay/settings.py
+++ django_pay/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'payments.middleware.DjangoPayExceptionsMiddleware',
 )
 
 ROOT_URLCONF = 'django_pay.urls'
